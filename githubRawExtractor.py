3f26a9ee14f0100e1ad909af996c3e528aeab776
chinmay18030 <77487404+chinmay18030@users.noreply.github.com>
2021-06-11 10:07:56 +0530

    Update githubRawExtractor.py

--- githubRawExtractor.py
+++ githubRawExtractor.py
@@ -1,23 +1,6 @@
-import requests 
+import githubRawxtractor
 
-
-class Extractor:
-    def __init__(self,githubrawlink):
-        self.githubrawlink = githubrawlink
-        self.content = requests.get(self.githubrawlink).text
-    
-    def extract(self):
-
-        return self.content
-
-    def save(self,filelocation):
-        f = open(filelocation,"w")
-        f.write(self.content)
-
-    def execute(self):
-        exec(self.content)
-
-
-extractor = Extractor("https://raw.githubusercontent.com/chinmay18030/handTracker/main/TrackHandModule.py")
-text = extractor.extract()
+ex = githubRawExtractor.Extractor("https://raw.githubusercontent.com/chinmay18030/handTracker/main/TrackHandModule.py")
+text = ex.extract()
 print(text)
+ex.execute()
